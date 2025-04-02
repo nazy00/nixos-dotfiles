ff41a00dde9512e34df2d8d08954b331292da80e
cybardev <50134239+cybardev@users.noreply.github.com>
2025-04-02 00:39:13 -0300

    fix: use Linux home directory instead of macOS

--- pkg/vscode.nix
+++ pkg/vscode.nix
@@ -1,11 +1,10 @@
 {
   pkgs,
   inputs,
-  userConfig,
+  extraArgs,
   ...
 }:
 let
-  userName = userConfig.username;
   system = pkgs.system;
   extensions = inputs.nix-vscode-extensions.extensions.${system};
   openvsxExt = with extensions.open-vsx; [
@@ -50,11 +49,11 @@ in
         "git.autofetch" = true;
         "git.confirmSync" = false;
         "git.enableSmartCommit" = true;
-        "idf.espIdfPath" = "/Users/${userName}/Documents/Git/esp/v5.4/esp-idf";
+        "idf.espIdfPath" = "${extraArgs.home}/Documents/Git/esp/v5.4/esp-idf";
         "idf.gitPath" = "git";
         "idf.hasWalkthroughBeenShown" = true;
         "idf.pythonInstallPath" = "/usr/bin/python3";
-        "idf.toolsPath" = "/Users/${userName}/Documents/Git/esp/.espressif";
+        "idf.toolsPath" = "${extraArgs.home}/Documents/Git/esp/.espressif";
         "nix.enableLanguageServer" = true;
         "nix.serverPath" = "nixd";
         "python.languageServer" = "Jedi";
