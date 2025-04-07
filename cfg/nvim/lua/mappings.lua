7b2ab8ed602dbcb8596f654bea6d32c7401318c6
cybardev <50134239+cybardev@users.noreply.github.com>
2025-04-07 20:15:27 -0300

    nvim add terminal toggle keymap

--- cfg/nvim/lua/mappings.lua
+++ cfg/nvim/lua/mappings.lua
@@ -1,10 +1,11 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- add yours here
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", ";", ":", { desc = "CMD enter command mode" })
+
+map({ "n", "t" }, "<leader>`", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "terminal toggle floating term" })
