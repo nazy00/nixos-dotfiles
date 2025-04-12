1ddc37bddb7648f105c2a0e2e8dad3c16f6c93d3
nazy00 <81646930+nazy00@users.noreply.github.com>
2025-04-12 15:43:55 -0300

    set wallpaper via dconf settings

--- pkg/bspwm.nix
+++ pkg/bspwm.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   dconf.settings = {
+    "org/gnome/desktop/background".picture-uri = "file://${../cfg/wallpaper.jpg}";
     "org/gnome/desktop/wm/preferences".theme = "Qogir-dark";
     # "org/gnome/desktop/interface" = {
     #   gtk-theme = "Qogir-dark";
