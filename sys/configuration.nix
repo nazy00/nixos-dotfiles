23d5a0521b0ee3a505e750a77b149c056ac9e454
nazy00 <81646930+nazy00@users.noreply.github.com>
2025-04-24 20:32:20 -0300

    add bluetooth support

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -174,6 +174,8 @@ in
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
   hardware.uinput.enable = true;
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
