718b91069d7182dd85f5b337aea3d656ac5c17e2
nazy00 <81646930+nazy00@users.noreply.github.com>
2026-08-02 07:29:41 -0300

    tqm 2.0

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -176,6 +176,10 @@ in
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
 
+  security.tpm2.enable = true;
+  security.tpm2.pkcs11.enable = true;  # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
+  security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
   hardware.uinput.enable = true;
